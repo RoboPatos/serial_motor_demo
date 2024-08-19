7bfc4352ff43634e73cb7f25721f02c0ffd07767
Natanael <amilnatanael@gmail.com>
2024-08-19 10:54:14 -0300

    VERSAO SEM PWM_MODE

--- serial_motor_demo/serial_motor_demo/driver.py
+++ serial_motor_demo/serial_motor_demo/driver.py
@@ -1,8 +1,6 @@
 import rclpy
 from rclpy.node import Node
 from serial_motor_demo_msgs.msg import MotorCommand
-from serial_motor_demo_msgs.msg import MotorVels
-from serial_motor_demo_msgs.msg import EncoderVals
 import time
 import math
 import serial
@@ -51,21 +49,17 @@ class MotorDriver(Node):
             self.motor_command_callback,
             10)
 
-        self.speed_pub = self.create_publisher(MotorVels, 'motor_vels', 10)
+        # Envia para GUI
+        # self.speed_pub = self.create_publisher(MotorVels, 'motor_vels', 10)
 
-        self.encoder_pub = self.create_publisher(EncoderVals, 'encoder_vals', 10)
+        # self.encoder_pub = self.create_publisher(EncoderVals, 'encoder_vals', 10)
         
 
         # Member Variables
 
         self.last_enc_read_time = time.time()
-        self.last_m1_enc = 0
-        self.last_m2_enc = 0
-        self.m1_spd = 0.0
-        self.m2_spd = 0.0
-
+        
         self.mutex = Lock()
-
 
         # Open serial comms
 
@@ -74,65 +68,17 @@ class MotorDriver(Node):
         print(f"Connected to {self.conn}")
         
 
-        
-
-
     # Raw serial commands
     
-    def send_pwm_motor_command(self, mot_1_pwm, mot_2_pwm):
-        #self.send_command(f"o {int(mot_1_pwm)} {int(mot_2_pwm)}")
-        self.send_command(f"5")
-
-    def send_feedback_motor_command(self, mot_1_ct_per_loop, mot_2_ct_per_loop):
-        self.send_command(f"m {int(mot_1_ct_per_loop)} {int(mot_2_ct_per_loop)}")
-
-    def send_encoder_read_command(self):
-        resp = self.send_command(f"e")
-        if resp:
-            return [int(raw_enc) for raw_enc in resp.split()]
-        return []
-
+    def send_pwm_motor_command(self, mot_1_speed, mot_2_speed):
+        self.send_command(f"o {int(mot_1_speed)} {int(mot_2_speed)}")
+        # para testar com velocidade fixa
+        # self.send_command(f"5") 
 
     # More user-friendly functions
 
     def motor_command_callback(self, motor_command):
-        if (motor_command.is_pwm):
-            self.send_pwm_motor_command(motor_command.mot_1_req_rad_sec, motor_command.mot_2_req_rad_sec)
-        else:
-            # counts per loop = req rads/sec X revs/rad X counts/rev X secs/loop 
-            scaler = (1 / (2*math.pi)) * self.get_parameter('encoder_cpr').value * (1 / self.get_parameter('loop_rate').value)
-            mot1_ct_per_loop = motor_command.mot_1_req_rad_sec * scaler
-            mot2_ct_per_loop = motor_command.mot_2_req_rad_sec * scaler
-            self.send_feedback_motor_command(mot1_ct_per_loop, mot2_ct_per_loop)
-
-    def check_encoders(self):
-        resp = self.send_encoder_read_command()
-        if (resp):
-
-            new_time = time.time()
-            time_diff = new_time - self.last_enc_read_time
-            self.last_enc_read_time = new_time
-
-            m1_diff = resp[0] - self.last_m1_enc
-            self.last_m1_enc = resp[0]
-            m2_diff = resp[1] - self.last_m2_enc
-            self.last_m2_enc = resp[1]
-
-            rads_per_ct = 2*math.pi/self.get_parameter('encoder_cpr').value
-            self.m1_spd = m1_diff*rads_per_ct/time_diff
-            self.m2_spd = m2_diff*rads_per_ct/time_diff
-
-            spd_msg = MotorVels()
-            spd_msg.mot_1_rad_sec = self.m1_spd
-            spd_msg.mot_2_rad_sec = self.m2_spd
-            self.speed_pub.publish(spd_msg)
-
-            enc_msg = EncoderVals()
-            enc_msg.mot_1_enc_val = self.last_m1_enc
-            enc_msg.mot_2_enc_val = self.last_m2_enc
-            self.encoder_pub.publish(enc_msg)
-
-
+        self.send_pwm_motor_command(motor_command.mot_1_speed, motor_command.mot_2_speed)
 
     # Utility functions
 
